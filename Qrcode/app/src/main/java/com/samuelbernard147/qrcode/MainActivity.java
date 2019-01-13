d4ee46a85aacd369d39b06873ef4b65178f94548
Samuel Bernard <samuelbernard147@gmail.com>
2019-01-13 21:19:01 +0700

    Add files via upload

--- Qrcode/app/src/main/java/com/samuelbernard147/qrcode/MainActivity.java
+++ Qrcode/app/src/main/java/com/samuelbernard147/qrcode/MainActivity.java
@@ -2,10 +2,16 @@ package com.samuelbernard147.qrcode;
 
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -14,16 +20,21 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.MultiFormatWriter   ;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class MainActivity extends AppCompatActivity {
     EditText inputan;
-    Button generate,btn_scan;
+    Button generate,btn_scan, btn_save;
     ImageView gambarhasil;
 
     @Override
@@ -35,6 +46,9 @@ public class MainActivity extends AppCompatActivity {
         inputan = (EditText)findViewById(R.id.edt_text);
         generate = (Button)findViewById(R.id.btn_generate);
         btn_scan = (Button)findViewById(R.id.btn_scan);
+        btn_save = (Button)findViewById(R.id.btn_save);
+        btn_save.setVisibility(View.GONE);
+
         gambarhasil = (ImageView)findViewById(R.id.img_hasil);
 
         //generate
@@ -48,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
                     BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
                     Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
                     gambarhasil.setImageBitmap(bitmap);
+//                    btn_save.setVisibility(View.VISIBLE);
 
                 }catch(WriterException e){
                     e.printStackTrace();
@@ -68,6 +83,7 @@ public class MainActivity extends AppCompatActivity {
                 integrator.initiateScan();
             }
         });
+
     }
 
     @Override
